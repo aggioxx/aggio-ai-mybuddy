5e82cd13cec9358e998d5576e81c280ff0119c98
guilhermeaggio <guilhermeaggio@outlook.com>
2024-07-09 18:46:09 -0300

    feat: first working code

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import os
 import json
 import time
 import weaviate
+from langchain_weaviate.vectorstores import WeaviateVectorStore
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
-from langchain.vectorstores import Weaviate
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from weaviate.collections.classes.config import Property, DataType
+import weaviate.classes.config as wc
+from openai import RateLimitError
 
 
 class AggiocorpData:
@@ -19,7 +20,9 @@ class AggiocorpData:
 
 
 def initialize_weaviate(weaviate_url):
-    weaviate_client = weaviate.connect_to_local(host=weaviate_url, port=8080)
+    weaviate_client = weaviate.connect_to_local(host=weaviate_url, port=8080, headers={
+        "X-OpenAI-Api-Key": os.getenv("OPENAI_API_KEY"),
+    })
     return weaviate_client
 
 
@@ -29,25 +32,10 @@ def read_json(file_path):
     return data
 
 
-def batch_insert(client, data, batch_size=100):
-    try:
-        with client.batch.fixed_size(batch_size=batch_size, concurrent_requests=4) as batch:
-            for item in data:
-                problem = AggiocorpData(
-                    problema=item['problema'],
-                    solucao=item['solução']
-                )
-
-                problem_data = {
-                    "problema": problem.problema,
-                    "solucao": problem.solucao
-                }
-
-                # Add to batch request
-                batch.add_object(properties=problem_data, collection="AggiocorpData")
-    finally:
-        print("Batch request completed.")
-        client.close()
+def insert_data(client, data):
+    aggiocorp = client.collections.get("AggiocorpData")
+    aggiocorp.data.insert_many(data)
+    print("Data inserted on weaviate.")
 
 
 def conversational_chat(query, chain):
@@ -62,56 +50,34 @@ def main():
     weaviate_url = os.getenv("WEAVIATE_URL")
     openai_api_key = os.getenv("OPENAI_API_KEY")
 
-    MyOpenAI = ChatOpenAI(temperature=0.5, model_name='gpt-3.5-turbo', openai_api_key="here-you-set-the-openai-api-key")
+    MyOpenAI = ChatOpenAI(temperature=0.5, model_name='gpt-3.5-turbo', openai_api_key=openai_api_key)
     weaviate_client = initialize_weaviate(weaviate_url)
     print("Clients initialized.")
 
     data = read_json(file_path)
-    # V3 weaviate client (deprecated)
-    # class_schema = {
-    #     "class": "HistoricalEvent",
-    #     "vectorizer": "text2vec-transformers",
-    #     "properties": [
-    #         {
-    #             "name": "Event",
-    #             "dataType": ["text"]
-    #         },
-    #         {
-    #             "name": "Year",
-    #             "dataType": ["int"]
-    #         },
-    #         {
-    #             "name": "Location",
-    #             "dataType": ["text"]
-    #         },
-    #         {
-    #             "name": "Significance",
-    #             "dataType": ["text"]
-    #         }
-    #     ]
-    # }
 
     try:
-        weaviate_client.collections.delete("aggiocorp")
+        weaviate_client.collections.delete("AggiocorpData")
     except:
         pass
     finally:
-        class_schema = {
-            "name": "AggiocorpData",
-            "description": "Schema for Aggiocorp data",
-            "vectorizer_config": "text2vec-transformers",
-            "properties": [
-                Property(name="problema", dataType=[DataType.TEXT]),
-                Property(name="solucao", dataType=[DataType.TEXT])
-            ]
-        }
-        weaviate_client.collections.create(**class_schema)
+        weaviate_client.collections.create(
+            name="AggiocorpData",
+            description="Schema for Aggiocorp data",
+            vectorizer_config=wc.Configure.Vectorizer.text2vec_openai(
+                model="ada",
+            ),
+            generative_config=wc.Configure.Generative.openai(),
+            properties=[
+                wc.Property(name="problema", data_type=wc.DataType.TEXT),
+                wc.Property(name="solucao", data_type=wc.DataType.TEXT)]
+        )
         print("Collection AggiocorpData was created.")
 
-    batch_insert(weaviate_client, data)
+    insert_data(weaviate_client, data)
 
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model='gpt-3.5-turbo')
-    vectorstore = Weaviate(weaviate_client, "AggiocorpData", "solucao", embeddings, by_text=False)
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model='text-embedding-ada-002')
+    vectordb = WeaviateVectorStore(weaviate_client, "AggiocorpData", "problema", embeddings)
     my_prompt = r"""
     Você é o chatbot da aggiocorp uma empresa de operações de TI e deve sempre responder como OpsBuddy um especialista em tecnologia.
     Considere o contexto definido entre [].
@@ -128,7 +94,7 @@ def main():
 
     chain = ConversationalRetrievalChain.from_llm(
         llm=MyOpenAI,
-        retriever=vectorstore.as_retriever(),
+        retriever=vectordb.as_retriever(),
         chain_type="stuff",
         verbose=True,
         combine_docs_chain_kwargs={'prompt': my_prompt}
@@ -138,7 +104,11 @@ def main():
         print("Olá, sou o opsbuddy, como posso te ajudar?")
         query = input("")
         start = time.time()
-        response = conversational_chat(query, chain)
+        try:
+            response = conversational_chat(query, chain)
+        except RateLimitError as e:
+            print(f"RateLimitError: {e} - Please check your OpenAI API quota and billing details.")
+            response = "O limite do cartão do dono do opsbuddy foi atingido :(."
         end = time.time()
         print("Tempo de resposta: ", end - start, "\tResposta:", response)
 
